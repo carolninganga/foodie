d58a48b9378fb10f0f8af2e34db9436e81fb46d0
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-21 12:28:11 -0400

    added models folder

---
+++ models/store.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.schema;
+
+const storeSchema = new Schema({ 
+    name: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String
+    },
+    street: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    state: {
+        type: String
+    },
+    zipcode: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    url: {
+        typr: String
+    }
+    });
+
+    const Store = mongoose.model('Store', storeSchema)
+    module.exports = Store;
