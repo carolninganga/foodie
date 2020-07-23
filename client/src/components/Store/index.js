88a3ab829a85a8122b129b2cdfdccce76947bce0
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-23 15:48:16 -0400

    added styling tolist item

--- client/src/components/Store/index.js
+++ client/src/components/Store/index.js
@@ -6,9 +6,12 @@ import "./style.css";
 function Store( { name, phone, street, city, state, zip_code, image, rating, url, Button }) {
     return (
         <ListItem> 
+            <h3 className="font-italic">{name}</h3>
+             <Col size="12 sm-4 md-2">
+                <img className="img-thumbnail img-fluid w-100" src={image} alt={name} />
+                </Col>
             <Row className="flex-wrap-reverse">
                 <Col size="md-8">
-                    <h3 className="font-italic">{name}</h3>
                     {phone && <h5 className="font-italic">{phone}</h5>} 
                 </Col>
                 <Col size="md-4">
@@ -23,16 +26,18 @@ function Store( { name, phone, street, city, state, zip_code, image, rating, url
 
             <Row>
                 <Col size="md-6">
-                    <p className="font-italic small">Located at {street, city, state, zip_code }</p>
+                    <p className="font-italic small"><i class="fas fa-map-marker-alt">{street} {city} {state} {zip_code }</i></p>
                 </Col>
             </Row>
             <Row>
                 <Col size="12 sm-4 md-2">
-                    <img className="img-thumbnail img-fluid w-100" src={image} alt={name} />
+                    {/* <img className="img-thumbnail img-fluid w-100" src={image} alt={name} /> */}
+                    <p className="rating">Rating:{rating}</p>
+
                 </Col>
-                <Col size="12 sm-8 md-10">
-                <p>{rating}</p>
-                </Col>
+                {/* <Col size="12 sm-8 md-10">
+                <p>Rating:{rating}</p>
+                </Col> */}
             </Row>
         </ListItem>
     )
